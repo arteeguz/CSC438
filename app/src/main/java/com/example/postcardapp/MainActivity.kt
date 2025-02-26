97179e6f710d70f24286bdcfb786d234e8232318
arteeguz <arteeguz@gmail.com>
2025-02-26 13:42:53 -0500

    Initial commit

---
+++ app/src/main/java/com/example/postcardapp/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.postcardapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.postcardapp.ui.theme.PostCardAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PostCardAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                }
+            }
+        }
+    }
+}
+@Composable
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.padding(8.dp)
+    ) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 150.sp,
+            color = Color.Blue
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp
+        )
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun PostcardPreview(){
+    PostCardAppTheme {
+        GreetingText(message = "What's up CSI", from = "Guz")
+    }
+}
